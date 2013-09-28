0331f4858350b67f1cd8aeee736e98a98b549f79
Cyriac Thomas <cyriac@compile.com>
2013-09-28 15:05:36 +0530

    Comments for readbility

--- tinypubsub.js
+++ tinypubsub.js
@@ -1,11 +1,17 @@
 function tpsChannel(options) {
 
 	this.options = options || {};
-
+    
+    //channel used for pubsub
 	this.channel = this.options.channel || [];
+
+    //channel subscribers
 	this.subscribers = this.options.subscribers || [];
+
+    //the minimum window for timeout, default: 0
 	this.subscriberTimeout = this.options.subscriberTimeout || 0;
 
+    //Timeout variable used for defferring callbacks
 	this.psTimeout = null;
 
 	this.init();
@@ -14,36 +20,43 @@ function tpsChannel(options) {
 tpsChannel.prototype = {
 
 	init: function() {
+        //call subscibers on object creation if channel not empty
 		if(this.channel.length > 0){
 			this.callSubscribers();
 		}
 	},
-
+    
+    //add subscribers
 	subscribe: function(callback) {
 		if( this.subscribers.indexOf(callback) == -1){
 			this.subscribers.push(callback);
 		}
 	},
-
+    
+    //remove subscribers
 	unsubscribe: function(callback) {
 		this.subscribers.splice(this.subscribers.indexOf(callback), 1);
 	},
-
+    
+    //overwrite channel (power tool)
 	update : function(objs) {
 		this.channel = objs;
 		this.callSubscribers();
 	},
 
+    //adding data to channel
 	push: function(obj) {
 		this.channel.push(obj);
 		this.callSubscribers();
 	},
 
+    //reset the channel
 	resetChannel: function(){
 		this.channel = [];
 	},
 
 	callSubscribers : function() {
+        //deferring the callback
 		if (this.psTimeout != null){
 			clearTimeout(this.psTimeout);
 		}
@@ -52,7 +65,8 @@ tpsChannel.prototype = {
 			accessor._callSubscribers(accessor.subscribers);
 		}, this.subscriberTimeout);
 	},
-
+    
+    //Actual call to subscibers
 	_callSubscribers : function(subscribers) {
 		var index = subscribers.length;
 		while(index--) {
